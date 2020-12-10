2c8f0cda84b4fcffb7834f69d9d44cbb3152c0a0
Ivan Proskuryakov <snowflax@ya.ru>
2020-12-10 17:48:41 +0300

    feat: Настроить Eslint и добавить .editorconfig

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+              Hello world
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './vendor/normalize.css'; // Подключили normalize.css
+import App from './components/App/App'; // Компонент приложения
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,7 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your App, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
