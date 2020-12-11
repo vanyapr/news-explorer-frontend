8df50fe1dc1121ca40d4565b3145314b52ac28b7
Ivan Proskuryakov <snowflax@ya.ru>
2020-12-11 17:32:48 +0300

    feat: Сверстать блоr "Об авторе"

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './About.css';
+import author from '../../images/author.jpg';
+import author2x from '../../images/author@2x.jpg';
 
 class About extends React.Component {
   render() {
     return (
       <>
         <section className='about'>
-          <h2 className="about__heading">Об авторе</h2>
+          <img className="about__image" src={author} srcSet={author2x} width='464' height='464' alt="Автор"/>
+          <div className="about__text">
+            <h2 className="about__heading">Об авторе</h2>
+            <p className="about__description">Меня зовут Ваня Проскуряков, я&nbsp;веб-разработчик из&nbsp;города Орехово-Зуево.
+              Я&nbsp;хорошо разбираюсь в&nbsp;HTML, CSS, Javascript, Node, PHP, Nginx, React и&nbsp;многом другом, касающемся
+              фронтенда, бекенда, и&nbsp;в&nbsp;целом веб-разработки.
+            </p>
+            <p className="about__description">На&nbsp;практикуме я&nbsp;стал разбираться во&nbsp;всём этом ещё лучше,
+              и&nbsp;получил прикольный диплом.
+            </p>
+          </div>
         </section>
       </>
     );
