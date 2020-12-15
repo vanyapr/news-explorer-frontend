463996593f30ca6967fe9e9e7864acc94ab86921
Ivan Proskuryakov <snowflax@ya.ru>
2020-12-15 14:11:00 +0300

    refactor: Реализовать адаптив хидера под 1024 пикселя

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Компоненты для роутинга и редиректа
 import NewsCardButton from '../NewsCardButton/NewsCardButton';
+import cardImage from '../../images/card-image.png';
 
 import './NewsCard.css';
 
@@ -8,12 +9,14 @@ class NewsCard extends React.Component {
   render() {
     return (
       <section className="card">
-        <img src="https://placehold.it/400x272" alt="Название карточки" className="card__image"/>
+        <div className="card__image-container">
+          <img src={cardImage} alt="Название карточки" className="card__image"/>
+        </div>
         <div className="card__text">
           <h3 className="card__name">
             <Link to='href://somewhere.ru' className='card__link'>«Три строки тайга»: новый фотопроект Игоря Шпиленка</Link>
           </h3>
-          <p className="card__description">В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков – охраняемых территорий, где и сегодня каждый может приобщиться к природе.</p>
+          <p className="card__description">В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков – охраняемых территорий, где и сегодня каждый может приобщиться к природе сто лет назад здесь начала складываться система национальных парков – охраняемых территорий, где и сегодня каждый может приобщиться к природе.</p>
         </div>
         <p className="card__source">Лента.ру</p>
         <p className="card__date"><time className="date_range small" dateTime="2019-2-08 00:00">2 августа, 2019</time></p>
